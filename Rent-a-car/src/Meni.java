69118b9fd534bf05acc5a839968de20107450b6d
ZigaKKolar <100192711+ZigaKKolar@users.noreply.github.com>
2022-04-23 21:17:01 +0200

    Admin meni
    Test GUI za admin meni.

---
+++ Rent-a-car/src/Meni.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Meni {
+    private JPanel meniFrame;
+    private JButton drzaveEDTButton;
+    private JButton krajiEDTButton;
+    private JButton uporabnikiEDTButton;
+    private JButton avtomobiliEDTButton;
+}
